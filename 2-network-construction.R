bd5dbd99d487cac81ed7c72d66343bc3d1538b19
SarthakSaluja1 <91469461+SarthakSaluja1@users.noreply.github.com>
2022-08-16 23:34:07 +0100

    Update network construction

--- 2-network-construction.R
+++ 2-network-construction.R
@@ -12,8 +12,13 @@ rm(list = ls())
 library(dplyr)
 library(data.table)
 library(igraph)
+library(ggplot2)
 
-## 1. Create a retweet network between all members of the tweet corpus ## 
+## Load in the data ## 
+
+all_tweets <- readRDS('tweets/all_tweets.rds')
+
+## 1. Create a re-tweet network between all members of the tweet corpus ## 
 
 # Create an edge list 
 
@@ -32,18 +37,77 @@ saveRDS(edges_retweets, 'edge_lists/edges_retweets.rds')
 
 ## 2. Basic Characteristics of network 
 
-# Re-tweet distribution
+# Summary characteristics 
 
-table(edges_retweets$count)
-hist(log(edge_df$count))
+summary(retweet_network) 
+no_nodes <- gorder(retweet_network) # 19055
+no_edges <- gsize(retweet_network) # 41701
 
-# Total number of tweets considered for further analysis 
+
+weighted_indeg <- graph.strength(retweet_network, mode = 'in')
+weighted_outdeg <- graph.strength(retweet_network, mode = 'out')
+nonweighted_indeg <- degree(retweet_network, mode = 'in')
+nonweighted_outdeg <- degree(retweet_network, mode = 'out')
+
+max_retweets_received <- max(weighted_indeg) 
+min_retweets_received <- min(weighted_indeg) 
+
+max_retweets_received # 4601
+min_retweets_received # 0
+
+max_retweets_sent <- max(weighted_outdeg) 
+min_retweets_sent <- min(weighted_outdeg) 
+
+max_retweets_sent # 5541
+min_retweets_sent # 0
+
+max_retweets_connections <- max(nonweighted_indeg)
+min_retweets_connections <- min(nonweighted_outdeg)
+
+max_retweets_connections # 1304
+min_retweets_connections # 0
+
+
+network_stats <- t(data.frame(no_nodes, 
+                              no_edges, 
+                              max_retweets_received, 
+                              max_retweets_sent, 
+                              max_retweets_connections))
+
+write.csv(network_stats, 'csv/network_stats.csv')
+
+# Degree distribution 
+
+agg_retweets_received <- edges_retweets |> 
+  group_by(retweet_author_id) |> 
+  summarize(sum = sum(count)) |> 
+  arrange(-sum) |> 
+  filter(sum > 10) |>
+  mutate(log_sum = log(sum)) |>
+  ggplot() + 
+  geom_density(aes(log_sum), 
+               adjust = 0.5) + 
+  xlab('Log of Aggregated retweets received') + 
+  theme_bw()
+
+agg_retweets_received
+
+ggsave('plots/Retweet Network/agg_retweets_received.png', 
+       agg_retweets_received, 
+       width = 5, 
+       height = 5)
+
+
+
+
+# Number of tweets containing user id
 
 ids <- unique(c(unique(edges_retweets$author_id), 
                 unique(edges_retweets$retweet_author_id)))
-length(ids)
 
 all_tweets |> 
   filter(author_id %in% ids) |> 
   summarise(n = n()) # 813083 tweets
+
+
 
